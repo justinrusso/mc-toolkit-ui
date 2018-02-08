6d4c70ce9821522ad472474926e7dc045b328295
Justin Russo <j.russo@outlook.com>
2018-02-08 07:19:42 -0500

    Fix e2e tests [ci skip]

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -7,8 +7,8 @@ describe('mc-toolkit-ui App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display home page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getMainHeaderText()).toEqual('MCToolKit');
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getMainHeaderText() {
+    return element(by.css('app-root h1.display-1')).getText();
   }
 }
