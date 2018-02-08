36393c8d5768f7a7c9d398bb4f61d4943b62e629
Justin Russo <j.russo@outlook.com>
2018-02-08 07:46:53 -0500

    Formatting

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {UIRouterModule} from '@uirouter/angular';
 import {APP_BASE_HREF} from '@angular/common';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -14,6 +15,7 @@ describe('AppComponent', () => {
       providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     }).compileComponents();
   }));
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
